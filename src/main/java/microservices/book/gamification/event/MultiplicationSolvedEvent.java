0e36bc0395066c6ffdf0e79479556c3ef07284b0
Jiwoong-Jung <jjw981119@naver.com>
2023-11-08 11:22:15 +0900

    Initial commit

---
+++ src/main/java/microservices/book/gamification/event/MultiplicationSolvedEvent.java
@@ -0,0 +1,36 @@
+package microservices.book.gamification.event;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+/**
+ * 시스템에서 곱셈 문제가 해결되었다는 사실을 모델링한 이벤트.
+ * 곱셈에 대한 컨텍스트 정보를 제공.
+ */
+//@RequiredArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
+//class MultiplicationSolvedEvent implements Serializable {
+class MultiplicationSolvedEvent {
+  private Long multiplicationResultAttemptId;
+  private Long userId;
+  private boolean correct;
+//  private final Long multiplicationResultAttemptId;
+//  private final Long userId;
+//  private final boolean correct;
+
+  public MultiplicationSolvedEvent(Long multiplicationResultAttemptId, Long userId, boolean correct) {
+    this.multiplicationResultAttemptId = multiplicationResultAttemptId;
+    this.userId = userId;
+    this.correct = correct;
+  }
+
+  public MultiplicationSolvedEvent() {
+
+  }
+}
